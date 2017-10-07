0abc8e1c4605be80ef3c9c4e2e178a65300bdd07
Joshua Smith <32210099+joshuaSmith2021@users.noreply.github.com>
2017-10-06 19:39:09 -0700

    Update schedule.js

--- script/schedule.js
+++ script/schedule.js
@@ -1,23 +1,3 @@
-var blocks = {
-  "regular":[
-    {"start":"7:45","end":"9:06","name":"Block 1","bold":false,"startHour":7,"startMinute":45,"endHour":9,"endMinute":6},
-    {"start":"9:06","end":"9:16","name":"Break","bold":true,"startHour":9,"startMinute":6,"endHour":9,"endMinute":16},
-    {"start":"9:21","end":"10:53","name":"Block 2","bold":false,"startHour":9,"startMinute":21,"endHour":10,"endMinute":53},
-    {"start":"10:58","end":"12:19","name":"Block 3","bold":false,"startHour":10,"startMinute":58,"endHour":12,"endMinute":19},
-    {"start":"12:19","end":"12:44","name":"Intervention","bold":false,"startHour":12,"startMinute":19,"endHour":12,"endMinute":44},
-    {"start":"12:44","end":"1:14","name":"Lunch","bold":true,"startHour":12,"startMinute":44,"endHour":13,"endMinute":14},
-    {"start":"1:19","end":"2:40","name":"Block 4","bold":false,"startHour":13,"startMinute":19,"endHour":14,"endMinute":40}
-  ],
-  
-  "articulation":[
-    {"start":"7:45","end":"8:53","name":"Block 1","bold":false,"startHour":7,"startMinute":45,"endHour":8,"endMinute":53},
-    {"start":"8:58","end":"10:14","name":"Block 2","bold":false,"startHour":8,"startMinute":58,"endHour":10,"endMinute":14},
-    {"start":"10:14","end":"10:34","name":"Break/Lunch","bold":true,"startHour":10,"startMinute":14,"endHour":10,"endMinute":34},
-    {"start":"10:39","end":"11:47","name":"Block 3","bold":false,"startHour":10,"startMinute":39,"endHour":11,"endMinute":47},
-    {"start":"11:52","end":"1:00","name":"Block 4","bold":false,"startHour":11,"startMinute":52,"endHour":13,"endMinute":0}
-  ]
-};
-
 // Maintenance Note: Change all `var data = blocks` to `var data = JSON.parse(blocks);` when editing code
 
 function getTimeAdv (date) {
@@ -27,7 +7,7 @@ function getTimeAdv (date) {
 function getNext () {
   var i;
   var cycle = true;
-  var data = blocks;
+  var data = JSON.parse(blocks);
   var days = [ 'Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday' ];
   var date = new Date();
   date.setHours(9);
@@ -53,7 +33,11 @@ function getNext () {
       if (currentTime < nextTime) {
         hoursLeft = Math.floor((nextTime - currentTime) / 60);
         minutesLeft = Math.floor((nextTime - (hoursLeft * 60)) - currentTime);
-        return 'There are ' + hoursLeft + ' hours left and ' + minutesLeft + ' minutes left until ' + render[i].name;
+        if (hoursLeft === 0) {
+          return minutesLeft + ' minutes until the end of ' + render[i].name;
+        } else if (hoursLeft !== 0) {
+          return hoursLeft + ' hours and ' + minutesLeft + ' until the end of ' + render[i].name;
+        }
       }
     }
   }
@@ -61,7 +45,7 @@ function getNext () {
 
 $(document).ready(function () {
   var days = [ 'Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday' ];
-  var data = blocks;
+  var data = JSON.parse(blocks);
   var i;
   var date = new Date();
   var render;
@@ -91,5 +75,3 @@ $(document).ready(function () {
     $('#text').text("No school today, it's " + days[date.getDay()] + "!");
   }
 });
-
-alert(getNext());
